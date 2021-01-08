c1da7c94e2b51407d1692b866af790fcd88d5bd2
shamaz332 <shamazsaeed332@gmail.com>
2021-01-09 03:08:39 +0500

    main stack ready for lambda

--- Cognito with Lambda/back/lib/back-stack.ts
+++ Cognito with Lambda/back/lib/back-stack.ts
@@ -1,9 +1,30 @@
-import * as cdk from '@aws-cdk/core';
-
+import * as cdk from "@aws-cdk/core";
+import * as cognito from "@aws-cdk/aws-cognito";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as iam from "@aws-cdk/aws-iam";
 export class BackStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const emailConf = new lambda.Function(this, "emailConfirmationLambda", {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: "emailConf.handler",
+      code: lambda.Code.fromAsset("functions"),
+    });
+
+    const userPool = new cognito.UserPool(this, "userPool", {
+      selfSignUpEnabled: true, //user can signup
+      autoVerify: { email: true }, //will send a verification email
+      signInAliases: { email: true }, //user can auth with email only
+      userVerification: {
+        emailSubject: "Verify your email to join our Community",
+        emailBody: "helo {username},Thanks for joining us",
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+        smsMessage: "helo {username},Thanks for joining us",
+      },
+      lambdaTriggers: {
+        preSignUp: emailConf,
+      },
+    });
   }
 }
